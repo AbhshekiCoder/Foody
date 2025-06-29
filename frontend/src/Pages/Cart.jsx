a93625e4271382eb261ed20fbd56318863564399
Abhicoder786 <117636060+abhiCoderShek@users.noreply.github.com>
2025-06-30 00:24:39 +0530

    first commit

--- frontend/src/Pages/Cart.jsx
+++ frontend/src/Pages/Cart.jsx
@@ -1,4 +1,4 @@
-himport axios from 'axios'
+import axios from 'axios'
 import React, { useEffect, useState } from 'react'
 import { useSelector } from 'react-redux'
 import url from '../misc/url.js'
